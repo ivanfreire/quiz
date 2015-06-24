4a277809f0a29ed78c2b50b3fed00f0aea6fb02a
ivanfreire <freirefi@hotmail.com>
2015-06-24 11:48:30 +0200

    HTTPS - HTTP Seguro.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
+var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
+var userController =    require('../controllers/user_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -24,7 +27,14 @@ router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
 router.put('/quizes/:quizId(\\d+)', quizController.update);
 router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
 
+/* comentarios */
+router.get('/quizes/:quizId(\\d+)/comments/new',                      commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',                         commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
-
+/* GET de autores del proyecto */
+router.get('/author', function(req, res) {
+  res.render('quizes/author', { author: 'Iván Freire', errors: [] });
+});
 
 module.exports = router;
